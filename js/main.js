f6760f1214858205c61bf12f9f2f79c370de66c6
sun1217shine <sun1217shine@gmail.com>
2020-04-03 03:28:37 +0900

    2020.04.03

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 window.onload = function() {
     scrolly();
+
+    $('#topBtn').click(function(){
+        $('html, body').stop().animate({
+            scrollTop : 0
+        });
+
+    });//top버튼 클릭
+
  }; 
